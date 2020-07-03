aa4e1a855fc3b4b0e13b04b9ca742206a6e62138
Sowmya Duvvuri <dsssri17@gmail.com>
2020-07-03 17:10:03 +0530

    Fifth commit

--- app/routes.js/eventRoutes.js
+++ app/routes.js/eventRoutes.js
@@ -1,2 +1,25 @@
 const express = require('express')
-const 
+const appConfig = require('../Configuration/appConfig')
+const auth = require('../middlewares/auth')
+
+module.exports.setRouter = (app) => {
+
+    let baseUrl = `${appConfig.apiVersion}/events`
+
+    app.get(`${baseUrl}/all`, auth.isAuthorized, getAllEvents)
+
+    app.get(`${baseUrl}/all/:userId`, auth.isAuthorized, getAllEventsOfSingleUser)
+
+    app.get(`${baseUrl}/:eventId`, auth.isAuthorized, getSingleEvent)
+
+    app.get(`${baseUrl}/count/all`, auth.isAuthorized, getCountOfAllEvents)
+
+    app.get(`${baseUrl}/count/:userId`, auth.isAuthorized, getAllEventCountOfSingleUser)
+
+    app.post(`${baseUrl}/create`, auth.isAuthorized, createEvent)
+
+    app.post(`${baseUrl}/delete/:eventId`, auth.isAuthorized, deleteEventByEventId)
+
+    app.put(`${baseUrl}/edit/:eventId`, auth.isAuthorized, editEvent)
+
+}
--- app/routes.js/userRoutes.js
+++ app/routes.js/userRoutes.js
@@ -1,37 +1,34 @@
 const express = require('express');
 const router = require.Router();
 const appConfig = require('../Configuration/appConfig');
-const Auth = require('../middlewares/auth');
+const auth = require('../middlewares/auth');
 
 module.exports.setRouter = (app) => {
 
 let baseUrl = `${appConfig.apiVersion}/user`
 
-app.get(`${baseUrl}/all`, Auth.isAuthorized, userController.getAllUsers)
+app.get(`${baseUrl}/all`, auth.isAuthorized, userController.getAllUsers)
 
-app.get(`${baseUrl}/all/normal`, Auth.isAuthorized, userController.getAllNormalUsers)
+app.get(`${baseUrl}/all/normal`, auth.isAuthorized, userController.getAllNormalUsers)
 
-app.get(`${baseUrl}/all/admin`, Auth.isAuthorized, userController.getAllAdminUsers)
+app.get(`${baseUrl}/all/admin`, auth.isAuthorized, userController.getAllAdminUsers)
 
-app.get(`${baseUrl}/:userId`, Auth.isAuthorized, userController.getUserByUserId)
+app.get(`${baseUrl}/:userId`, auth.isAuthorized, userController.getUserByUserId)
 
-app.post(`${baseUrl}/signup`, Auth.isAuthorized, userController.signUpFunction)
+app.post(`${baseUrl}/signup`, auth.isAuthorized, userController.signUpFunction)
 
-app.post(`${baseUrl}/login`, Auth.isAuthorized, userController.loginFunction)
+app.post(`${baseUrl}/login`, auth.isAuthorized, userController.loginFunction)
 
-app.post(`${baseUrl}/logout`, Auth.isAuthorized, userController.logoutFunction)
+app.post(`${baseUrl}/logout`, auth.isAuthorized, userController.logoutFunction)
 
-app.post(`${baseUrl}/:userId/delete`, Auth.isAuthorized, userController.deleteUser)
+app.post(`${baseUrl}/delete/:userId`, auth.isAuthorized, userController.deleteUser)
 
-app.post(`${baseUrl}/recoverymail/password`, Auth.isAuthorized, userController.recoveryMail)
+app.post(`${baseUrl}/recoverymail/password`, auth.isAuthorized, userController.recoveryMail)
 
-app.post(`${baseUrl}/rest/paswrd/:authToken`, Auth.isAuthorized, userController.resetPassword)
+app.post(`${baseUrl}/restpassword/:authToken`, auth.isAuthorized, userController.resetPassword)
 
-app.get(`${baseUrl}/all/count`, Auth.isAuthorized, userController.getAllUsersCount)
+app.get(`${baseUrl}/count/normal`, auth.isAuthorized, userController.getAllNormalUsersCount)
 
-app.get(`${baseUrl}/all/normal/count`, Auth.isAuthorized, userController.getAllNormalUsersCount)
-
-app.get(`${baseUrl}/all/admin/count`, Auth.isAuthorized, userController.getAllAdminUsersCount)
+app.get(`${baseUrl}count/admin`, auth.isAuthorized, userController.getAllAdminUsersCount)
 
 }
-
